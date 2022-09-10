9f05ba5c676a889d74232c32c07e94e226a50b80
pgeovany <pgeovany@ic.ufrj.br>
2022-09-10 20:10:00 -0300

    chore: implement wi-fi repository

--- src/repositories/wifiRepository.ts
+++ src/repositories/wifiRepository.ts
@@ -3,3 +3,32 @@ import { Wifis } from '@prisma/client';
 
 export type WifiInsertData = Omit<Wifis, 'id'>;
 export type WifiRequestData = Omit<Wifis, 'id' | 'userId'>;
+
+async function create(data: WifiInsertData) {
+  await prisma.wifis.create({ data });
+}
+
+async function findById(id: number, userId: number) {
+  return await prisma.wifis.findFirst({ where: { id, userId } });
+}
+
+async function findByUserId(userId: number) {
+  return await prisma.wifis.findMany({ where: { userId } });
+}
+
+async function findByUserIdAndTitle(userId: number, title: string) {
+  return await prisma.wifis.findUnique({
+    where: {
+      userId_title: {
+        userId,
+        title,
+      },
+    },
+  });
+}
+
+async function remove(id: number) {
+  await prisma.wifis.delete({ where: { id } });
+}
+
+export { create, findByUserIdAndTitle, findById, findByUserId, remove };
