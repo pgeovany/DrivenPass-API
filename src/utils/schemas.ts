fb8bff9ce40a6876abd1af3dfb008a19d1a434bc
pgeovany <pgeovany@ic.ufrj.br>
2022-09-12 17:11:26 -0300

    chore: add regex to sign-up schema to force passwords to have at least 8 characters, one uppercase letter, one lowercase letter, one number and one special character

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -5,7 +5,12 @@ const joi = JoiImport.extend(DateExtension);
 
 const signUpSchema = joi.object({
   email: joi.string().required(),
-  password: joi.string().required(),
+  password: joi
+    .string()
+    .pattern(/^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$/, {
+      invert: true,
+    })
+    .required(),
 });
 
 const credentialSchema = joi.object({
